0a0fbb577701ea336cd63a6b71ecac24f3a552bd
Danil <danil.present@mail.ru>
2018-04-09 21:27:50 +0800

    Doable new library "ezprint"! Use lib: Print ('Hello world') = n ('Hello world'))

--- WikiParser.py
+++ WikiParser.py
@@ -1,34 +1,31 @@
 
-# class="infobox"
+#class="infobox"
 from bs4 import BeautifulSoup
 import requests
 import os
 import pandas as pd
 import openpyxl
-from ezprint import p
+from ezprint import p #print('Hello, World') = p('Hello, World')
 
-url = 'https://ru.wikipedia.org/wiki/'
+url = 'https://ru.wikipedia.org/wiki/' #const
 
 c1 = []
 c2 = []
 
+
 def search():
 	global url
 	value = input('Input request: ')
-	url = url + value
-
-
-# def p(value):
-# 	print(value)
+	url = url + value #search quest
 
 
 def cls():
-	os.system('cls')
+	os.system('cls') #cls console
 
 
 def get_html(url):
 	r = requests.get(url)
-	return r.text
+	return r.text #Get html 
 
 
 def parse():
@@ -40,10 +37,9 @@ def parse():
 
 	table = soup.find('table', class_ = "infobox")
 	try:
-
 		trs = table.find_all('tr')
 	except:
-		print('Search Error!')
+		p('Search Error!')
 		exit()
 	for i in trs:
 		try:
@@ -51,23 +47,24 @@ def parse():
 			cols = cols.replace('\n', '')
 			c1.append(cols)
 		except:
-			c1.append('None')
+			c1.append('NoneData')
 		try:
 			cols1 = i.find('td').getText()
 			cols1 = cols1.replace('\n', '')
 			c2.append(cols1)
 		except:
-			c2.append('None')
+			c2.append('NoneData')
 
 	df = pd.DataFrame({
 		'Quests' : c1,
 		'Info'   : c2
 		})
-	print(df)
-
-	p('dfffffffffffff')
+	p(df)
 	df.to_excel('wiki.xlsx')
+
+
 if __name__ == '__main__':
 	search()
 	cls()
 	parse()
+	p('Save page!')
